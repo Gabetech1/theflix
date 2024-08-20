574b16209a7fcb8d18cc7c6cc16925172d9c8b92
Gabetech1 <43847702+Gabetech1@users.noreply.github.com>
2024-08-20 12:21:06 +0000

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
  // url = 'http://127.0.0.1:8000/api';
-  url = 'https://gabeapi.siteshowcase.top/api';
+  url = 'https://api.flixshipping.com/api';
 
   constructor(private http: HttpClient) { }
 
